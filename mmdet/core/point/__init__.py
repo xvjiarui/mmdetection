ce570014d46932a70ae6d46db76de4068313ef24
Jiarui XU <xvjiarui0826@gmail.com>
2020-03-03 00:36:34 +0800

    force fix

--- mmdet/core/point/__init__.py
+++ mmdet/core/point/__init__.py
@@ -1,7 +1,7 @@
+from .utils import denormalize, point_sample, roi2point, roi_point2img_coord
 from .point_target import point_target
 from .sampler import (get_uncertain_point_coords,
                       get_uncertain_point_coords_with_randomness)
-from .utils import denormalize, point_sample, roi2point, roi_point2img_coord
 
 __all__ = [
     'roi2point', 'denormalize', 'point_target',
