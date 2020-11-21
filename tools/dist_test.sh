b51d228fdbd185d2d196d62198071af1a6fe864d
Jiarui XU <xvjiarui0826@gmail.com>
2020-11-20 18:14:11 -0800

    fixed ENV in sh

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -6,5 +6,6 @@ CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
 
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -5,5 +5,6 @@ PYTHON=${PYTHON:-"python"}
 CONFIG=$1
 GPUS=$2
 
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
