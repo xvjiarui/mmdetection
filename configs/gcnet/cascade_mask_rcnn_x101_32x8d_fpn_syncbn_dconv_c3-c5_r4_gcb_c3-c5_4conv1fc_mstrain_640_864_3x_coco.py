fb1a37b38979e913c5c622363e6293906bc33b63
Jiarui XU <xvjiarui0826@gmail.com>
2020-11-21 10:16:21 -0800

    change to 3x

--- configs/gcnet/cascade_mask_rcnn_x101_32x8d_fpn_syncbn_dconv_c3-c5_r4_gcb_c3-c5_4conv1fc_mstrain_640_864_3x_coco.py
+++ configs/gcnet/cascade_mask_rcnn_x101_32x8d_fpn_syncbn_dconv_c3-c5_r4_gcb_c3-c5_4conv1fc_mstrain_640_864_3x_coco.py
@@ -92,7 +92,7 @@ train_pipeline = [
         with_seg=False),
     dict(
         type='Resize',
-        img_scale=[(1600, 400), (1600, 1400)],
+        img_scale=[(1440, 640), (1440, 864)],
         multiscale_mode='range',
         keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
@@ -104,5 +104,5 @@ train_pipeline = [
 data = dict(
     samples_per_gpu=2, workers_per_gpu=2, train=dict(pipeline=train_pipeline))
 # learning policy
-lr_config = dict(step=[16, 19])
-total_epochs = 20
+lr_config = dict(step=[28, 34])
+total_epochs = 36
